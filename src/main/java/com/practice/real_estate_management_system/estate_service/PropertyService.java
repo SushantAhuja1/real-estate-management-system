e30ba5640f755fd23eceac1d00149942f32f4859
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-02-01 15:12:18 +0530

    Implemented addProperty logic in Service Layer using ModelMapper

--- src/main/java/com/practice/real_estate_management_system/estate_service/PropertyService.java
+++ src/main/java/com/practice/real_estate_management_system/estate_service/PropertyService.java
@@ -1,10 +1,21 @@
 package com.practice.real_estate_management_system.estate_service;
+import com.practice.real_estate_management_system.entity_dto.PropertyDTO;
+import com.practice.real_estate_management_system.estate_entity.Property;
 import com.practice.real_estate_management_system.estate_repository.PropertyRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PropertyService {
     private final PropertyRepository propertyRepository;
+    private final ModelMapper modelMapper;
+
+    //add-property-function
+    public PropertyDTO addProperty(PropertyDTO propertyDTO) {
+        Property property = modelMapper.map(propertyDTO, Property.class);
+        property = propertyRepository.save(property);
+        return modelMapper.map(property, PropertyDTO.class);
+    }
 }
