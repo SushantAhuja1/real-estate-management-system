e4bc6a18b42178c2542d90d296082560dd4d567d
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-02-01 13:46:27 +0530

    Entity Layer completed and port conflict resolved

---
+++ src/main/java/com/practice/real_estate_management_system/estate_entity/Property.java
@@ -0,0 +1,34 @@
+package com.practice.real_estate_management_system.estate_entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "properties")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private double price;
+    @Column(nullable = false)
+    private String ownerName;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PropertyType type;
+    @ElementCollection
+    @CollectionTable(name = "property_services")
+    private List<String> property_services;
+    @ElementCollection
+    @CollectionTable(name = "property_landmarks")
+    private List<String> property_landmarks;
+}
---
+++ src/main/java/com/practice/real_estate_management_system/estate_entity/PropertyType.java
@@ -0,0 +1,7 @@
+package com.practice.real_estate_management_system.estate_entity;
+
+public enum PropertyType {
+    FLAT,
+    PG,
+    PLOT
+}
