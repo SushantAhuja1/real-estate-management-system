86b57863c83439fa6cc91b9d11dcb8468270c3d5
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-02-01 13:15:59 +0530

    Initial commit

---
+++ src/test/java/com/practice/real_estate_management_system/RealEstateManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.real_estate_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
