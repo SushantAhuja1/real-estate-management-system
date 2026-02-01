00959857401ecf1fb71ee1f6855d1d3552a1b9ce
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-02-01 15:04:50 +0530

    Added DTO and did the model mapper thing for easy conversion task

--- src/main/java/com/practice/real_estate_management_system/RealEstateManagementSystemApplication.java
+++ src/main/java/com/practice/real_estate_management_system/RealEstateManagementSystemApplication.java
@@ -1,7 +1,9 @@
 package com.practice.real_estate_management_system;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RealEstateManagementSystemApplication {
@@ -9,4 +11,8 @@ public class RealEstateManagementSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RealEstateManagementSystemApplication.class, args);
 	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
