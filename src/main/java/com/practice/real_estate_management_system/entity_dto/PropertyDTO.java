76424254abdfb2f4ba0a601c25e97e33d9d8c93d
Sushant Ahuja <sushant.ahuja@highwirepress.com>
2026-02-08 11:32:24 +0530

    Added Bean Validation to PropertyDTO
    Added spring-boot-starter-validation dependency to pom.xml.
    
    Implemented @NotBlank for address/owner and @Min(0) for price in PropertyDTO.

--- src/main/java/com/practice/real_estate_management_system/entity_dto/PropertyDTO.java
+++ src/main/java/com/practice/real_estate_management_system/entity_dto/PropertyDTO.java
@@ -3,6 +3,8 @@ package com.practice.real_estate_management_system.entity_dto;
 import com.practice.real_estate_management_system.estate_entity.PropertyStatus;
 import com.practice.real_estate_management_system.estate_entity.PropertyType;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +15,13 @@ import java.util.List;
 @NoArgsConstructor
 public class PropertyDTO {
 
+    @NotBlank(message = "Address is mandatory")
     private String address;
 
+    @Min(value = 0, message = "Price cannot be negative")
     private double price;
 
+    @NotBlank(message = "Owner name is mandatory")
     private String ownerName;
 
     private PropertyType type;
